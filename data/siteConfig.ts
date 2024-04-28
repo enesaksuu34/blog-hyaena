4ab57d0fb19a172cf2308eeb592bbc74e45c305a
Enes Aksu <88115318+enesaksuu34@users.noreply.github.com>
2024-04-28 23:17:17 +0300

    Update siteConfig.ts

--- data/siteConfig.ts
+++ data/siteConfig.ts
@@ -1,11 +1,11 @@
 import type { SiteConfig } from "@/lib/types";
 const siteConfig: SiteConfig = {
-  avatar: "https://img.hyaena.co.uk/hlogo.png",
+  avatar: "/hlogo.png",
   siteUrl: "https://blog.hyaena.co.uk",
   siteName: "Hyaena Blog",
   siteDescription:
     "Empowering Your Digital Journey with Expert Insights and Solutions",
-  siteThumbnail: "https://img.hyaena.co.uk/hlogo.png",
+  siteThumbnail: "/hlogo.png",
   nav: [
     { label: "Posts", href: "/posts" },
     { label: "About", href: "/about" },
