4b642115dc480178b318c5a8abf3464ae7a5352f
Enes Aksu <88115318+enesaksuu34@users.noreply.github.com>
2024-05-22 15:16:26 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,9 @@
-// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 import Document, { DocumentContext } from "next/document";
 import { cx } from "@/lib/utils";
+
+const GA_TRACKING_ID = 'G-CLL1CRSFF1'; // Replace with your GA tracking ID
+const GTM_ID = 'GTM-KNS9TMJJ'; // Replace with your GTM ID
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -15,7 +17,7 @@ class MyDocument extends Document {
         <Head>
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-CLL1CRSFF1`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script
             dangerouslySetInnerHTML={{
@@ -29,6 +31,17 @@ class MyDocument extends Document {
               `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `,
+            }}
+          />
         </Head>
         <body
           className={cx(
@@ -36,6 +49,14 @@ class MyDocument extends Document {
             "dark:bg-gray-900 dark:text-gray-50"
           )}
         >
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+                <iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
